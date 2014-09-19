54fe78fbc8935a84e4c56a195f8dc6d345d458ea
Anton Zimin <anton@saritasa.com>
2014-09-19 20:39:00 +0800

    Implemented tktx tool.

---
+++ SharpDX.Tools/tktex/Program.cs
@@ -0,0 +1,37 @@
+﻿using SharpDX.Toolkit.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tktex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: tktext <texture file>");
+                return;
+            }
+
+            try
+            {
+                string fileName = args[0];
+                string newName = Path.Combine(Path.GetDirectoryName(fileName), Path.GetFileNameWithoutExtension(fileName) + ".tktx");
+
+                var image = Image.Load(fileName);
+                image.Save(newName, ImageFileType.Tktx);
+
+                Console.WriteLine(fileName + " => " + newName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
