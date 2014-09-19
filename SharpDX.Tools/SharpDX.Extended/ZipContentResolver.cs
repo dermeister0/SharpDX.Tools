2cf916f6bc24e957a978e71ee7552d28d06875c4
Anton Zimin <anton@saritasa.com>
2014-09-19 20:59:44 +0800

    Implemented ZipContentResolver.

---
+++ SharpDX.Tools/SharpDX.Extended/ZipContentResolver.cs
@@ -0,0 +1,56 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using SharpDX.Toolkit.Content;
+using System;
+
+namespace SharpDX.Extended
+{
+    public class ZipContentResolver : IContentResolver, IDisposable
+    {
+        ZipFile zipFile;
+
+        bool isDisposed;
+
+        public ZipContentResolver(ZipFile zipFile)
+        {
+            this.zipFile = zipFile;
+        }
+
+        public bool Exists(string assetName)
+        {
+            return zipFile.GetEntry(ConvertPath(assetName)) != null;
+        }
+
+        public System.IO.Stream Resolve(string assetName)
+        {
+            var entry = zipFile.GetEntry(ConvertPath(assetName));
+            if (entry == null)
+                throw new AssetNotFoundException(assetName);
+
+            return zipFile.GetInputStream(entry);
+        }
+
+        string ConvertPath(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return path;
+
+            return path.Replace('\\', '/');
+        }
+    
+        public void Dispose()
+        {
+ 	        Dispose(true);
+        }
+
+        protected virtual void Dispose(bool isDisposing)
+        {
+            if (!isDisposing && !isDisposed)
+            {
+                if (zipFile != null)
+                    zipFile.Close();
+                zipFile = null;
+                isDisposed = true;
+            }
+        }
+    }
+}
